52d077808117fc3dba8d5b1e93c70933b9e37126
esantonja <enrique.santonja@travelport.com>
2017-03-12 19:25:22 +0100

    Not able to update the barchart

--- src/www/js/components/measure-time-form.jsx
+++ src/www/js/components/measure-time-form.jsx
@@ -8,13 +8,22 @@ module.exports =  React.createClass({
     getInitialState: function() {
         return {
             destinationHost: "",
-            times: 1
+            times: 1,
+            data: [
+                {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+                {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+                {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+                {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+                {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+                {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+                {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+            ],
         };
     },
 
     onDestinationHostChange: function(e) {
         this.setState({
-            destinationHost: e.target.value
+            destinationHost: e.target.value            
         });
     },
 
@@ -25,7 +34,20 @@ module.exports =  React.createClass({
     },
 
     checkTimes: function(item) {
-        console.log("Call service.")
+        console.log("Call service.");
+        let data = this.state.data;
+        var data = [
+                {name: 'Page A', uv: 5000, pv: 9400, amt: 1200},
+                {name: 'Page B', uv: 6000, pv: 8398, amt: 2310},
+                {name: 'Page C', uv: 7000, pv: 7800, amt: 3490},
+                {name: 'Page D', uv: 8780, pv: 6908, amt: 4500},
+                {name: 'Page E', uv: 9890, pv: 5800, amt: 5681},
+                {name: 'Page F', uv: 1390, pv: 4800, amt: 6400},
+                {name: 'Page G', uv: 2490, pv: 3300, amt: 2200},
+            ]; 
+        this.setState({
+            data: data
+        });
     },
 
     render: function() {
@@ -37,7 +59,7 @@ module.exports =  React.createClass({
                        <input id="destinationHost" value={this.state.times} onChange={this.onTimesChange} />
                        <button type="button" onClick={this.checkTimes}>Check response time</button>
                    </form>
-                   <BarcharResult />
+                   <BarcharResult data={this.state.data}/>
                 </div>;
     }
 });
